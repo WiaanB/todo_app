92aded3eff5ecd3fac386ce62b2a079dc4c10324
wiaan <wiaan.botha@spandigital.com>
2024-02-07 21:11:19 +0200

    lint fixed on the day list component and added some boilerplate for my intern

--- src/components/DayList/index.tsx
+++ src/components/DayList/index.tsx
@@ -1,21 +1,25 @@
 import React, { useState, useEffect } from 'react';
 
-const DAYS = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const DAYS_OF_WEEK = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
 const DayList: React.FC = () => {
     const [days, setDays] = useState<string[]>([]);
     const [selectedDay, setSelectedDay] = useState<number | null>(null);
 
     useEffect(() => {
-        let dayArr = DAYS.slice(new Date().getDay(), 7);
-        if (dayArr.length < 7) dayArr = dayArr.concat(DAYS.slice(0, 7 - dayArr.length))
+        let dayArr = DAYS_OF_WEEK.slice(new Date().getDay(), 7);
+        if (dayArr.length < 7) dayArr = dayArr.concat(DAYS_OF_WEEK.slice(0, 7 - dayArr.length));
         
         setDays(dayArr);
         setSelectedDay(0);
     },[])
 
+    // const handleDayClick = (index: number) => {
+    //     setSelectedDay(index);
+    // };
+
     return <div>
-        {days.at(selectedDay ?? 0)}
+        <p>The selected day is: {days[selectedDay ?? 0]}</p>
     </div>
 };
 
